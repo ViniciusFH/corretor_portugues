7e2e2d3c29bc17e99b7eacf054d42849c384a515
ViniciusFH <vinicius.heleno@hotmail.com>
2019-05-07 23:57:04 -0300

    .

--- bruce.py
+++ bruce.py
@@ -1,6 +1,5 @@
 from corretor import *
-from formal_arvore import formal
-from informal_arvore import informal
+from grande_dicionario import dicionario
 import time
 import unidecode
 import re
@@ -8,12 +7,28 @@ repeater = True
 while repeater:
     palavras = [re.sub("[^a-zA-Z]+","",re.sub('[\"\']+',"",word)) for word in unidecode.unidecode(input("Insira a frase: ")).split()]
     temp_1=time.time()
-    erradas,informais = identifica(palavras,formal,informal)
-    sugestoes,niveis = corretor_frase(erradas,formal)
-    print ("")
-    peso(palavras,sugestoes,niveis,informais)
-    temp_2=time.time()
-    demora=float(format(temp_2-temp_1, '.2f'))
-    print ("")
-    print ("Demorou %s segundos" %(demora))
-    print ("")
+    erradas,informais = identifica(palavras,dicionario)
+    if len(erradas)+len(informais)==0:
+        temp_2=time.time()
+        demora=float(format(temp_2-temp_1, '.2f'))
+        score = 1.0
+        print ("")
+        print ("Corretas: "+str(len(palavras)))
+        print ("Completamente erradas: Nenhuma")
+        print ("Com erro nivel um: Nenhuma")
+        print ("Com erro nivel dois: Nenhuma")
+        print ("Informais: Nenhuma")
+        print ("")
+        print ("Score: "+str(float(format(score, '.2f'))))
+        print ("")
+        print ("Demorou %s segundos" %(demora))
+        print ("")
+    else:
+        sugestoes,niveis = corretor_frase(erradas,dicionario)
+        print ("")
+        peso(palavras,sugestoes,niveis,informais)
+        temp_2=time.time()
+        demora=float(format(temp_2-temp_1, '.2f'))
+        print ("")
+        print ("Demorou %s segundos" %(demora))
+        print ("")
--- corretor.py
+++ corretor.py
@@ -80,23 +80,18 @@ def troca(entrada):
 # pertencem ao dicionario, e devolve uma lista com elas
 
 
-def identifica(palavras, dicionario, informal):
+def identifica(palavras, dicionario):
     
     
     erradas = []
     informais = []
+
     for i in palavras:
-            if len(i)<5:
-                    if i not in dicionario["quatro"]:
-                            if i in informal["quatro"]:
-                                    informais.append(i)
-                            else:
-                                    erradas.append(i)
-            elif i not in dicionario[i[0]][i[1]][i[2]][i[3]]:
-                    if i in informal[i[0]][i[1]][i[2]][i[3]]:
-                            informais.append(i)
-                    else:
+            if i not in dicionario["formal"]:
+                    if i not in dicionario["informal"]:
                             erradas.append(i)
+                    else:
+                            informais.append(i)
 
     return erradas, informais                                          
 
@@ -136,42 +131,17 @@ def corretor_palavra(palavra, dicionario):
 
 
         for word in inserir:
-                
-                if len(word)<5:
-                        if word in dicionario["quatro"]:
-                                possiveis.append(word)
-                else:
-                        if word in dicionario[word[0]][word[1]][word[2]][word[3]]:
-                                possiveis.append(word)
-                
+                if word in dicionario["formal"] or word in dicionario["informal"]:
+                        possiveis.append(word)
         for word in excluir:
-                
-                if len(word)<5:
-                        if word in dicionario["quatro"]:
-                                possiveis.append(word)
-                else:
-                        if word in dicionario[word[0]][word[1]][word[2]][word[3]]:
-                                possiveis.append(word)
-                
+                if word in dicionario["formal"] or word in dicionario["informal"]:
+                        possiveis.append(word)
         for word in substituir:
-
-                if len(word)<5:
-                        if word in dicionario["quatro"]:
-                                possiveis.append(word)
-                else:
-                        if word in dicionario[word[0]][word[1]][word[2]][word[3]]:
-                                possiveis.append(word)
-
+                if word in dicionario["formal"] or word in dicionario["informal"]:
+                        possiveis.append(word)
         for word in trocar:
-
-                if len(word)<5:
-                        if word in dicionario["quatro"]:
-                                possiveis.append(word)
-                else:
-                        if word in dicionario[word[0]][word[1]][word[2]][word[3]]:
-                                possiveis.append(word)
-
-
+                if word in dicionario["formal"] or word in dicionario["informal"]:
+                        possiveis.append(word)
 
         if len(possiveis)==0:
 
@@ -180,40 +150,24 @@ def corretor_palavra(palavra, dicionario):
                 for i in inserir:
                         lista = insere(i,alfabeto)
                         for word in lista:
-                                if len(word)<5:
-                                        if word in dicionario["quatro"] and word not in possiveis:
-                                                possiveis.append(word)
-                                else:
-                                        if word in dicionario[word[0]][word[1]][word[2]][word[3]] and word not in possiveis:
-                                                possiveis.append(word)
+                                if word in dicionario["formal"] or word in dicionario["informal"]:
+                                        possiveis.append(word)
                 for i in excluir:
                         lista = exclui(i)
                         for word in lista:
-                                if len(word)<5:
-                                        if word in dicionario["quatro"] and word not in possiveis:
-                                                possiveis.append(word)
-                                else:
-                                        if word in dicionario[word[0]][word[1]][word[2]][word[3]] and word not in possiveis:
-                                                possiveis.append(word)
+                                if word in dicionario["formal"] or word in dicionario["informal"]:
+                                        possiveis.append(word)
                 for i in substituir:
                         lista = substitui(i,alfabeto)
                         for word in lista:
-                                if len(word)<5:
-                                        if word in dicionario["quatro"] and word not in possiveis:
-                                                possiveis.append(word)
-                                else:
-                                        if word in dicionario[word[0]][word[1]][word[2]][word[3]] and word not in possiveis:
-                                                possiveis.append(word)
+                                if word in dicionario["formal"] or word in dicionario["informal"]:
+                                        possiveis.append(word)
 
                 for i in trocar:
                         lista = troca(i)
                         for word in lista:
-                                if len(word)<5:
-                                        if word in dicionario["quatro"] and word not in possiveis:
-                                                possiveis.append(word)
-                                else:
-                                        if word in dicionario[word[0]][word[1]][word[2]][word[3]] and word not in possiveis:
-                                                possiveis.append(word)
+                                if word in dicionario["formal"] or word in dicionario["informal"]:
+                                        possiveis.append(word)
 
 
         if len(possiveis)==0:
@@ -225,7 +179,7 @@ def corretor_palavra(palavra, dicionario):
 
 
 
-# classifica em niveis o quanto uma frase está próxima do dicionário
+# score de uma frase
 
 
 def peso(palavras, sugestoes, niveis, informais):
@@ -254,84 +208,3 @@ def peso(palavras, sugestoes, niveis, informais):
         print ("")
         print ("Score: "+str(float(format(score, '.2f'))))
         
-
-
-        """
-
-        print(informais)
-
-        if len(sugestoes)==0:
-                print("Tudo certo. Nota 1.0")
-                return
-
-        if len(palavras)>15:
-                print("Frase muito grande. Nota 0")
-                return
-
-        impossiveis = [impossivel for impossivel in sugestoes if len(sugestoes[impossivel])==0]
-        if len(impossiveis)>0:
-                print("Pelo menos 1 palavra impossível de ser corrigida. Nota 0.")
-                return
-        
-        if len(palavras)==len(sugestoes):
-                print("Todas as palavras estão erradas. Nota 0.")
-                return
-        
-        if len(palavras)>2:
-
-                possiveis = [possivel for possivel in sugestoes if len(sugestoes[possivel])>0]
-                certas = len(palavras)-len(sugestoes)
-
-                if (certas*100)/len(palavras)>=80:
-                        print("%s%s das palavras estão certas. Nota 0.7" %(int((certas*100)/len(palavras)),"%"))
-                        return
-                if (certas*100)/len(palavras)>=50:
-                        print("%s%s das palavras estão certas. Nota 0.5" %(int((certas*100)/len(palavras)),"%"))
-                        return
-                if (certas*100)/len(palavras)<50:
-                        print("%s%s das palavras estão certas. Nota 0.3" %(int((certas*100)/len(palavras)),"%"))
-                        return
-        else:
-                print("Frase com duas palavras e uma delas está errada. Nota 0.3")
-                return
-
-
-
-
- 
-        print ("%s palavras" %(len(palavras)))
-        print ("%s erradas" %(len(sugestoes)))
-        print ("%s pode(m) ser corrigida(s)" %(len(possiveis)))
-        print ("%s em Nivel 1 de correçao" %(len(nivel_um)))
-        print ("%s em Nivel 2 de correçao" %(len(nivel_dois)))
-        print ("%s nao pode(m) ser corrigida(s)" %(len(impossiveis)))
-                                
-
-                nivel_um = [nivel for nivel in niveis if nivel==1]
-                nivel_dois = [nivel for nivel in niveis if nivel==2]
-
-
-        if len(sugestoes)<len(palavras):
-                
-
-                if len(impossiveis)==0:
-                        classificacao = "Nivel B: %s de %s tokens sao palavras, e todos podem ser corrigidos em primeira instancia." %((len(palavras)-len(possiveis)),len(palavras))
-
-                elif len(possiveis)==0:
-                        classificacao = "Nivel D: %s de %s tokens sao palavras, mas nenhum pode ser corrigido em primeira instancia." %((len(palavras)-len(impossiveis)),len(palavras))
-                
-                else:
-                        classificacao = "Nivel C: %s de %s tokens sao palavras, %s pode(m) ser corrigido(s) em primeira instancia, e %s nao." %((len(palavras)-(len(possiveis)+len(impossiveis)),len(palavras),len(possiveis),len(impossiveis)))
-        
-
-        else:
-                if len(impossiveis)==0:
-                        classificacao = "Nivel E: Nenhum token é uma palavra, mas todos podem ser corrigidos em primeira instancia."
-                elif len(possiveis)==0:
-                        classificacao = "Nivel G: Nenhum token é uma palavra e nenhum pode ser corrigido em primeira instancia."
-                else:
-                        classificacao = "Nivel F: nenhum token é uma palavra mas %s pode(m) ser corrigido(s) em primeira instancia" %(len(possiveis))
-
-
-        print (classificacao)
-"""
